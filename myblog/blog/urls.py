fd432079bc67c450b6c8633c7102ca1c9e161620
Ah-oy <58558581+ei-blue@users.noreply.github.com>
2023-06-17 14:45:17 -0700

    blog template

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.post_list, name='post_create'),
+    path('create/', views.post_create, name='post_create'),
     path('', views.post_list, name='post_list'),
 
 ]
