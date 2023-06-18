fd7ed94a154435ecfee15e4309cc6dd0795e86cd
KevinISun <114264552+KevinISun@users.noreply.github.com>
2023-06-17 22:07:02 -0700

    removing extraneous files

--- OpenAI_API/OAI_calling.py
+++
@@ -1,5 +0,0 @@
-import os
-import openai
-openai.organization = "org-uIoNOrZkKMd0w2hOoyl8x6N6"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('api/chat/', views.chat_api, name='chat_api'),
 ]
